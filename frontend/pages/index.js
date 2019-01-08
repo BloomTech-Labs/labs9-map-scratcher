f38d8423b81a64ecdf909ba560870121c568f543
samsisle <samko9522@gmail.com>
2019-01-08 14:54:17 -0800

    Add about and index pages [1]

---
+++ frontend/pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default () => (
+  <div>
+    <h1>Bacpaca!</h1>
+    <p>
+      <Link>
+        <a href="/about">About</a>
+      </Link>
+    </p>
+  </div>
+);
