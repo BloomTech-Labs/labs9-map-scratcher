46beed4d4e57ec62a988d26e0158952268b9d5d1
JacobABrennan <jacobabrennan@gmail.com>
2019-01-10 14:42:46 -0500

    Connected Yoga to Primsa

--- backend/resolvers/Query.js
+++ backend/resolvers/Query.js
@@ -1,10 +1,19 @@
+
+
+//== Root Query ================================================================
+
+//-- Construct Query -----------------------------
 const Query = {
-  hello: () => 'Hello',
-  // country: (parent, args, context) => {
-  //   return context.prisma.countries()
-  // },
+    hello: () => 'Hello',
+    // country: (parent, args, context) => {
+    //   return context.prisma.countries()
+    // },
+    user: (parent, args, context) => {
+        return context.prisma.user({id: args.id});
+    }
 };
 
+//-- Export --------------------------------------
 module.exports = {
-  Query
+    Query
 };
