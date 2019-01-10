16437726a4dd9ec7a0d1d1141d6a146701f94837
samsisle <samko9522@gmail.com>
2019-01-09 23:46:54 -0800

    Add root _app page [1]

---
+++ frontend/pages/_app.js
@@ -0,0 +1,24 @@
+import App, { Container } from 'next/app';
+
+export default class MyApp extends App {
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    pageProps.query = ctx.query;
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    );
+  }
+}
