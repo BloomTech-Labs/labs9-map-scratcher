985cc53b870490d1571ad3997bdc945cf158d15f
Lola <lolahef@gmail.com>
2019-01-09 20:23:36 -0500

    Regenerated schema

--- backend/prisma/generated/prisma-client/index.js
+++ backend/prisma/generated/prisma-client/index.js
@@ -5,7 +5,15 @@ var typeDefs = require("./prisma-schema").typeDefs;
 
 var models = [
   {
+    name: "Country",
+    embedded: false
+  },
+  {
     name: "User",
+    embedded: false
+  },
+  {
+    name: "Visit",
     embedded: false
   }
 ];
