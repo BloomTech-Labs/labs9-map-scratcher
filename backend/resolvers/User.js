e470aa5dbaae7fe9bad197b4c8104b3407a27057
TRIF3X <hoessly.conner@gmail.com>
2019-01-10 18:49:25 -0600

    user resolver mostly complete, missing visit resolver

--- backend/resolvers/User.js
+++ backend/resolvers/User.js
@@ -1,6 +1,11 @@
 const User = {
     id: (parent) => parent.id,
-    name: () => 'cool'
+    name: (parent) => parent.name,
+    nickname: (parent) => parent.nickname,
+    email: (parent) => parent.email,
+    visits: (parent, args, context) => {
+        return context.prisma.visit({user: parent.name})
+    }
 }
 
 module.exports = {
