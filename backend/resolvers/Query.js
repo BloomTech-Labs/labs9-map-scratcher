1ac9b5284589464ed8b0f8bd3e1a0f51ab889d37
samsisle <samko9522@gmail.com>
2019-01-10 22:56:08 -0800

    Update root resolvers Query [1]

--- backend/resolvers/Query.js
+++ backend/resolvers/Query.js
@@ -1,19 +1,26 @@
-
-
 //== Root Query ================================================================
 
 //-- Construct Query -----------------------------
 const Query = {
-    hello: () => 'Hello',
-    // country: (parent, args, context) => {
-    //   return context.prisma.countries()
-    // },
-    user: (parent, args, context) => {
-        return context.prisma.user({id: args.id});
-    }
-};
+  hello: () => 'Hello',
+  // country: (parent, args, context) => {
+  //   return context.prisma.countries()
+  // },
+  user: (parent, args, context) => {
+    return context.prisma.user({ id: args.id })
+  },
+  country: (parent, args, context) => {
+    return context.prisma.country({ id: args.id })
+  },
+  visit: (parent, args, context) => {
+    return context.prisma.visit({ id: args.id })
+  },
+  visits: (parent, args, context) => {
+    return context.prisma.visits()
+  }
+}
 
 //-- Export --------------------------------------
 module.exports = {
-    Query
-};
+  Query
+}
