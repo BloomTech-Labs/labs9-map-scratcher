52ec90c98ce946c707aa7328c407021bc8011e16
samsisle <samko9522@gmail.com>
2019-01-15 11:01:06 -0800

    Add cookie-session config along with passport related middleware [2]

--- backend/services/passport/middleware.js
+++ backend/services/passport/middleware.js
@@ -1,6 +1,9 @@
 // NODE MODULES, DEPENDENCIES
 // ==============================================
 const passport = require('passport')
+const cookieSession = require('cookie-session')
+
+const keys = require('../../config/keys')
 
 // PASSPORT MIDDLEWARE
 // ==============================================
@@ -8,6 +11,13 @@ const passport = require('passport')
 // ({ express }) using es6 destructuring syntax
 // ==============================================
 module.exports = ({ express }) => {
+  express.use(
+    cookieSession({
+      name: 'Backpaca',
+      key: keys.cookieKey,
+      maxAge: 60 * 60 * 1000 // 1 hour
+    })
+  )
   express.use(passport.initialize())
   express.use(passport.session())
 }
