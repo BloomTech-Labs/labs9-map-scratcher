5a81f46059abc3e9a966df3c9f0788029c5a29c5
samsisle <samko9522@gmail.com>
2019-01-15 13:17:47 -0800

    Update passport js [5]

--- backend/services/passport/passport.js
+++ backend/services/passport/passport.js
@@ -3,51 +3,40 @@
 const passport = require('passport')
 const TwitterStrategy = require('passport-twitter').Strategy
 
+const keys = require('../../config/keys')
+
 // TWITTER PASSPORT STRATEGY
 // ==============================================
 module.exports = prisma => {
-  // const user = await prisma;
-  // console.log(Object.keys(prisma))
-  // const userQuery1 = { id: 'cjqpxk83t000o0829p7mr6qto' }
-  // const query = `
-  //   query {
-  //     user(id: "cjqpxk83t000o0829p7mr6qto") {
-  //       id
-  //       name
-  //     }
-  //    }
-  // `
-  // const unknownPromise = prisma.user(userQuery1)
-  // const result = await unknownPromise
-  // console.log('[user]', result)
   passport.serializeUser((user, done) => done(null, user.id))
-
   passport.deserializeUser(async (id, done) => {
     const user = await prisma.user({ id })
     done(null, user)
   })
-  
-  // passport.use(
-  //   new TwitterStrategy(
-  //     {
-  //       consumerKey: keys.twitterConsumerKey,
-  //       consumerSecret: keys.twitterConsumerSecret,
-  //       callbackURL: '/auth/twitter/callback',
-  //       proxy: true
-  //     },
-  //     async (token, tokenSecret, profile, done) => {
-  //       const existingUser = await User.findOne({ twitterId: profile.id });
-  //       if (existingUser) {
-  //         done(null, existingUser);
-  //       } else {
-  //         const user = await new User({
-  //           username: profile.displayName,
-  //           twitterId: profile.id,
-  //           avatar: profile._json.profile_image_url
-  //         }).save();
-  //         done(null, user);
-  //       }
-  //     }
-  //   )
-  // );
+
+  passport.use(
+    new TwitterStrategy(
+      {
+        consumerKey: keys.twitterConsumerKey,
+        consumerSecret: keys.twitterConsumerSecret,
+        callbackURL: '/auth/twitter/callback',
+        proxy: true
+      },
+      async (token, tokenSecret, profile, done) => {
+        const existingUser = await prisma.user({
+          twitterHandle: profile.displayName
+        })
+        if (existingUser) {
+          done(null, existingUser)
+        } else {
+          const user = await prisma
+            .user({
+              twitterHandle: profile.displayName
+            })
+            .save()
+          done(null, user)
+        }
+      }
+    )
+  )
 }
