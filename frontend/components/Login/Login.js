b4e6bb678013dc6069dbcf62e65d23efbf487813
samsisle <samko9522@gmail.com>
2019-01-15 21:13:29 -0800

    Update Twitter button to have oauth href [1]

--- frontend/components/Login/Login.js
+++ frontend/components/Login/Login.js
@@ -1,33 +1,41 @@
 import React, { Component } from 'react'
 import { Button, Icon, Input } from 'semantic-ui-react'
 
-export default class Login extends Component {
+const login =
+  process.env.NODE_ENV === 'production'
+    ? 'https://backpaca-yoga.herokuapp.com/auth'
+    : 'http://localhost:4000/auth'
 
-	render() {
-		return (
-            <form>
-                <div>
-                    <h3>Login User</h3>
-                    <Input
-                    type='text'
-                    placeholder='username'
-                    name='username'
-                    value=''
-                    />
-                    <Input
-                    type='text'
-                    placeholder='password'
-                    name='password'
-                    value=''
-                    />
-                    <div>
-                    <Button color='twitter'>
-                        <Icon name='twitter' /> Login with Twitter
-                    </Button>
-                    <Button>Register</Button>
-                    </div>
-                </div>
-            </form>
-		)
-	}
+export default class Login extends Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <div>
+            <h3>Login User</h3>
+            <Input
+              type="text"
+              placeholder="username"
+              name="username"
+              value=""
+            />
+            <Input
+              type="text"
+              placeholder="password"
+              name="password"
+              value=""
+            />
+            <div>
+              <Button>Register</Button>
+            </div>
+          </div>
+        </form>
+        <a href={`${login}/twitter`}>
+          <Button color="twitter">
+            <Icon name="twitter" /> Login with Twitter
+          </Button>
+        </a>
+      </div>
+    )
+  }
 }
