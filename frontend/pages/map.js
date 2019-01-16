dac910302adf05fa8fec7bfc30cee2a0d87dd309
samsisle <samko9522@gmail.com>
2019-01-16 13:44:01 -0800

    Add semantic-ui spinner with dimming effect [1]

--- frontend/pages/map.js
+++ frontend/pages/map.js
@@ -1,24 +1,31 @@
-import Link from 'next/link';
+import Link from 'next/link'
 import dynamic from 'next/dynamic'
+import { Dimmer, Loader } from 'semantic-ui-react'
+
 import MapIndex from '../components/Map/mapPage/index.js'
 
 const DynamicMap = dynamic(() => import('../components/Map/Map'), {
+  loading: () => (
+    <Dimmer active>
+      <Loader size="large" />
+    </Dimmer>
+  ),
   ssr: false
 })
 
 export default () => (
   <div>
-    <nav style={{display:'flex',justifyContent:'space-between'}}>
-      <div className='nav-left' style={{display:'flex'}}>
-        <Link href='/'>
+    <nav style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <div className="nav-left" style={{ display: 'flex' }}>
+        <Link href="/">
           <a>Home </a>
         </Link>
         <div>> </div>
-        <Link href='/map'>
+        <Link href="/map">
           <a>Map</a>
         </Link>
       </div>
-      <div className='nav-right'>
+      <div className="nav-right">
         <a>Sign Out</a>
       </div>
     </nav>
