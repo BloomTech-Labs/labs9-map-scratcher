57b876aedf84c295b373525d35bc72f4a167f10d
TRIF3X <thewizardofcrypto@gmail.com>
2019-01-16 14:32:48 -0600

    legend component

--- frontend/pages/map.js
+++ frontend/pages/map.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import dynamic from 'next/dynamic'
 import MapIndex from '../components/Map/mapPage/index.js'
+import Legend from '../components/Map/mapPage/legend.js'
 
 const DynamicMap = dynamic(() => import('../components/Map/Map'), {
   ssr: false
@@ -23,6 +24,9 @@ export default () => (
       </div>
     </nav>
     <MapIndex />
+    <div>
     <DynamicMap />
+    <Legend />
+    </div>
   </div>
 )
