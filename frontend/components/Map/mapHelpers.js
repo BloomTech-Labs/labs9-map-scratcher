d94cb5cfacbf7ea6a46f679da649623d718cee9c
kyranmccann <kyran.mccann@gmail.com>
2019-01-17 01:52:42 -0800

    moving dummy data to another file

--- frontend/components/Map/mapHelpers.js
+++ frontend/components/Map/mapHelpers.js
@@ -1,109 +1,5 @@
 
 //dummy data for use during testing
-const mapColorVisits = [
-        {
-          "level": 3,
-          "country": {
-            "name": "Hungary",
-            "code": "HUN"
-          }
-        },
-        {
-          "level": 1,
-          "country": {
-            "name": "China",
-            "code": "CHN"
-          }
-        },
-        {
-          "level": 1,
-          "country": {
-            "name": "Belize",
-            "code": "BLZ"
-          }
-        },
-        {
-          "level": 3,
-          "country": {
-            "name": "Sweden",
-            "code": "SWE"
-          }
-        },
-        {
-          "level": 1,
-          "country": {
-            "name": "Costa Rica",
-            "code": "CRI"
-          }
-        }
-      ];
-
-//dummy data for use during testing
-const dataWeGet = [
-  {
-    "id": "cjqpxk83t000o0829p7mr6qto",
-    "name": "Ally Paca",
-    "visits": [
-      {
-        "id": "cjr0da23s002r0847hq4jx8g5",
-        "level": 4,
-        "country": {
-          "id": "cjqy9e28y00i20840rwy5l1ti",
-          "name": "Hungary",
-          "code": "HUN"
-        }
-      }
-    ]
-  },
-  {
-    "id": "cjqt5d9ox00sl0894ur6k9qza",
-    "name": "Albert Paca",
-    "visits": [
-      {
-        "id": "cjr0cztae000j0847e0nwmvzr",
-        "level": 2,
-        "country": {
-          "id": "cjqy9e28y00i20840rwy5l1ti",
-          "name": "Hungary",
-          "code": "HUN"
-        }
-      },
-      {
-        "id": "cjr0d27uq00110847j71uybat",
-        "level": 3,
-        "country": {
-          "id": "cjqy9e0wl007q08402wv4d6qk",
-          "name": "Argentina",
-          "code": "ARG"
-        }
-      },
-      {
-        "id": "cjr0d2qdf001a0847w8j99vk9",
-        "level": 2,
-        "country": {
-          "id": "cjqy9e38t00pq0840hc5u8n0f",
-          "name": "Nepal",
-          "code": "NPL"
-        }
-      }
-    ]
-  },
-  {
-    "id": "cjqy1r16l00200831bj7hpkwk",
-    "name": "Lorelei",
-    "visits": [
-      {
-        "id": "cjqya1gq300zp0840l4468h9s",
-        "level": 2,
-        "country": {
-          "id": "cjqy9e0tw007e08407aebjb7r",
-          "name": "Aland",
-          "code": "ALA"
-        }
-      }
-    ]
-  }
-];
 
 //gets the country polygon information from the country code
 function getFeature(data, code) {
@@ -130,8 +26,6 @@ const tileURLs = {
 
 
 module.exports = {
-  mapColorVisits,
-  dataWeGet,
   getFeature,
   fixData
 }
