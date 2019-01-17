436f96c316856e4f004ecb5e7c7317c305f01c8d
kyranmccann <kyran.mccann@gmail.com>
2019-01-17 12:42:00 -0800

    starting to include apollo queries

--- frontend/pages/map.js
+++ frontend/pages/map.js
@@ -1,8 +1,9 @@
 import dynamic from 'next/dynamic'
 import { Dimmer, Loader } from 'semantic-ui-react'
+import { USERVISITS_QUERY, FRIENDSVISITS_QUERY } from '../services/queries'
 
-import MapIndex from '../components/Map/mapPage/index.js'
-import Legend from '../components/Map/mapPage/legend.js'
+import MapIndex from '../components/MapHeader/MapIndex.js'
+import Legend from '../components/MapLegend/Legend.js'
 
 const DynamicMap = dynamic(() => import('../components/Map/Map'), {
   loading: () => (
@@ -13,12 +14,45 @@ const DynamicMap = dynamic(() => import('../components/Map/Map'), {
   ssr: false
 })
 
-export default () => (
-  <div>
-    <MapIndex />
-    <div>
-    <DynamicMap />
-    <Legend />
-    </div>
-  </div>
-)
+export default () => {
+      <div>
+        <MapIndex />
+        <div>
+          <Query query={FRIENDSVISITS_QUERY} variable={{id}}>
+          {({ loading, error, data }) => {
+              if (loading) return <div>Fetching</div>
+              if (error) return <div>Error</div>
+              const friendsVisitsData = data.user.friends; 
+              return (
+                  <div>
+                      {friendsData.map(user => <div key={user.id}>{user.name} {user.email}</div>)}
+                  </div>
+              )
+          }}
+          </Query>
+          <DynamicMap
+            borderData={this.state.borderData}
+          />
+          <Legend />
+        </div>
+      </div>
+    );
+  }
+
+  // Apollo Query handlers
+  receivedFriendBorderData = (queryResponse) => {
+    // Get data from response;
+    const loading = queryResponse.loading;
+    const error = queryResponse.error;
+    const data = data;
+    const friendsData = data;
+    friendsData.sanityCheck = true;
+    // Handle loading and error states
+    if(loading) { return null}
+    if(error) { return null}
+    // Handle Receiving of Data
+    return this.setState({
+      borderData: friendsData
+    });
+  }
+}
