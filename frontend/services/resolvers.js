0b258864bdd37c4939a93b99f9973f71ed84609b
kyranmccann <kyran.mccann@gmail.com>
2019-01-22 01:16:32 -0800

    updated mutation resolver

--- frontend/services/resolvers.js
+++ frontend/services/resolvers.js
@@ -1,5 +1,5 @@
 import gql from 'graphql-tag';
-import { QUERY_CLIENT_TRAVELS } from './requests';
+import { QUERY_CLIENT_VIEWFRIEND } from './requests';
 
 // export const typeDefs = gql`
 //   extend type Query {
@@ -10,19 +10,21 @@ import { QUERY_CLIENT_TRAVELS } from './requests';
 //   }
 // `
 
+export const resolvers = {
+  Mutation: {
+    viewFriend: (_obj, { id }, {cache}) => {
 
+      const data = {
+        viewingFriend: true,
+        friendId: id
+      };
+      cache.writeData({ data });
 
-export const viewFriend = (_obj, { id }, {cache}) => {
-  const query = QUERY_CLIENT_TRAVELS;
-  const currentState = cache.readQuery({ query });
-  console.log('the state before', currentState);
+      const query = QUERY_CLIENT_VIEWFRIEND;
+      const currentState = cache.readQuery({ query });
+      console.log('the state after', currentState);
 
-  cache.writeData({ data: { viewingFriend: true, friendId: id}});
-
-  const queryTwo = QUERY_CLIENT_TRAVELS;
-  const roundTwo = cache.readQuery({ queryTwo });
-
-  console.log('the state after', roundTwo);
-
-  return null;
+      return data;
+    }
+  }
 }
