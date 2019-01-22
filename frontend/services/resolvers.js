ded5c44f57079c599b0bb59f4c0fccf9ad4b390a
kyranmccann <kyran.mccann@gmail.com>
2019-01-22 09:10:49 -0800

    update toggle resolver

--- frontend/services/resolvers.js
+++ frontend/services/resolvers.js
@@ -1,5 +1,5 @@
 import gql from 'graphql-tag';
-import { QUERY_CLIENT_VIEWFRIEND } from './requests';
+import { QUERY_CLIENT_VIEWFRIEND, QUERY_CLIENT_LOGGED } from './requests';
 
 // export const typeDefs = gql`
 //   extend type Query {
@@ -23,6 +23,18 @@ export const resolvers = {
       const query = QUERY_CLIENT_VIEWFRIEND;
       const currentState = cache.readQuery({ query });
       console.log('the state after', currentState);
+
+      return data;
+    },
+    toggleLoggedIn: (_obj, args, {cache}) => {
+      const query = QUERY_CLIENT_LOGGED;
+      const currentState = cache.readQuery({ query });
+      console.log(currentState);
+
+      const data = {
+        isLoggedIn: !currentState.isLoggedIn
+      }
+      cache.writeData({data});
 
       return data;
     }
