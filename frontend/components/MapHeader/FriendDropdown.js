143b1a8d7079042040a3a7424da3ff19bd9db3ba
kyranmccann <kyran.mccann@gmail.com>
2019-01-23 00:24:54 -0800

    added comments

--- frontend/components/MapHeader/FriendDropdown.js
+++ frontend/components/MapHeader/FriendDropdown.js
@@ -11,22 +11,28 @@ export default class FriendDropdown extends Component {
   }
 
   render() {
+    //query retrieves the userId from the apollo cache
     return (
       <Query query={QUERY_CLIENT_PROFILE}>
         {({ loading: loadingUserId, data: { userId }}) => {
+          //query returns a fragment containing a second query for the user's friend, using the id that was retrieved in the previous query
           return (
             <React.Fragment>
               <Query query={QUERY_FRIENDS_HEADER} variables={{ id: userId }}>
                 {({ loading: loadingFriends, data: { friends }}) => {
+                  //takes the array of friends retrieved and maps through it to set the value of the dropdown options
                   const friendsList = friends.map(friend => {
                     return {
                       text: friend.name,
                       value: friend.id
                     }
                   })
+                  //adds the user to the top of the dropdown options array.
                   friendsList.unshift({ text: 'My Travels', value: userId })
+                  //query returns a mutation that checks if the id passed is the same as the user id, and if not sets the apollo cache values for the friend being viewed and the related boolean. 
                   return (
-                    <Mutation mutation={MUTATION_VIEWINGFRIEND_TRAVELS}>
+                    <Mutation mutation={MUTATION_VIEWINGFRIEND_TRAVELS}
+                    >
                     {(viewFriend, { data }) => (
                       <Dropdown
                         placeholder="My Travels"
