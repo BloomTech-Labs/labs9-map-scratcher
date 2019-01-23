a1bf759e84b58246d32ebbceeb987f85895ccc26
kyranmccann <kyran.mccann@gmail.com>
2019-01-23 00:25:04 -0800

    added comments

--- frontend/components/MapHeader/ViewBordersCheckbox.js
+++ frontend/components/MapHeader/ViewBordersCheckbox.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
-import { Mutation } from 'react-apollo'
-import { Checkbox } from 'semantic-ui-react'
+import { Query, Mutation } from 'react-apollo';
+import { Checkbox } from 'semantic-ui-react';
+import { QUERY_CLIENT_VIEWFRIEND, MUTATION_TOGGLE_BORDERS } from '../../services/requests';
 
 export default class ViewBordersCheckbox extends Component {
   render() {
+    //returns a query to check the apollo cache for the viewingFriend boolean in order to set the proper label
     return (
-        <div className="mapIndex_friend-checkbox">
-          <Checkbox type="checkbox" name="showFriendsTravels" value="" />
-          &nbsp;&nbsp;Show Friends' Travels
-        </div>
+      <Query query={QUERY_CLIENT_VIEWFRIEND}>
+      {({ loading, data}) => {
+        let label = "";
+        if (data.viewingFriend) {
+          //label for when a friend is being viewed.
+          label = "Show Your Travels";
+        }
+        if (!data.viewingFriend) {
+          //label for when the user's map is being viewed.
+          label="Show Friend's Travels"
+        }
+        //query returns a mutation that will toggle the viewBorders boolean in the apollo cache. 
+        return (
+          <Mutation mutation={MUTATION_TOGGLE_BORDERS}>
+          {(toggleBorders, { data }) => (
+            <div className="mapIndex_friend-checkbox">
+              <Checkbox
+              type="checkbox" name="showFriendsTravels"
+              value=""
+              onChange={toggleBorders}
+              label={label}
+              />
+            </div>
+          )}
+          </Mutation>
+        )
+      }}
+      </Query>
     )
   }
 }
