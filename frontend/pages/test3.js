b9c3baaf3f0b801d374bd80252cdfed75b596409
kyranmccann <kyran.mccann@gmail.com>
2019-01-23 11:15:11 -0800

    testing

--- frontend/pages/test3.js
+++ frontend/pages/test3.js
@@ -1,19 +1,20 @@
-
-
 //==============================================================================
 
 //-- Dependencies --------------------------------
 import dynamic from 'next/dynamic';
 import { Dimmer, Loader } from 'semantic-ui-react';
-import { QUERY_CLIENT_TRAVELS, QUERY_USERVISITS_TRAVELS, QUERY_FRIENDSVISITS_TRAVELS, MUTATION_TOGGLE_SOMETHING } from '../services/requests.js';
+import { QUERY_CLIENT_TRAVELS, QUERY_USERVISITS_TRAVELS, QUERY_FRIENDSVISITS_TRAVELS, QUERY_CLIENT_MODAL } from '../services/requests.js';
 import { Query, Mutation, ApolloConsumer } from 'react-apollo';
 import React, { Component } from 'react';
-import QueryMap from '../components/Map/QueryMap.js';
+import MapHeader from '../components/MapHeader/MapHeader.js';
+import Legend from '../components/MapLegend/Legend.js';
+import CountryModal from '../components/CountryViewModal/CountryModal'
 import { fixData } from '../components/Map/mapHelpers';
 
 //-- Constants -----------------------------------
 const testUserId = "cjqt5c95y00s40894zs7m6q4v";
 
+//------------------------------------------------
 const DynamicMap = dynamic(() => import('../components/Map/StaticMap'), {
   loading: () => (
     <Dimmer active>
@@ -23,15 +24,18 @@ const DynamicMap = dynamic(() => import('../components/Map/StaticMap'), {
   ssr: false
 });
 
+
+//== React lifecycle methods ===================================================
+
 export default class extends Component {
     constructor(props) {
         super(props);
     }
 
     render() {
-        // return this.makeQueriesAndRenderMap();
         return (
           <React.Fragment>
+            <MapHeader />
             <Query query={QUERY_CLIENT_TRAVELS}>
             {({ loading: loadinguser, data }) => {
               const localState = data;
@@ -40,16 +44,15 @@ export default class extends Component {
               console.log({id});
               return (
                 <Query query={QUERY_USERVISITS_TRAVELS} variables={{id}}>
-                {({ loading: loadingVisits, data: { user }} ) => {
-                  console.log(data)
+                {({ loading: loadingVisits, data} ) => {
+                  console.log('uservisits', data)
                   let visitsUser = [];
                   visitsUser.push(data.user);
-                  console.log(visitsUser);
-                  // visitsUser = (fixData(visitsUser))
+                  visitsUser = (fixData(visitsUser))
                   return (
                     <Query query={QUERY_FRIENDSVISITS_TRAVELS}variables={{id}}>
                       {({ loading: loadingFriendVisits, data: { friends }}) => {
-                        console.log(friends);
+                        console.log('friendvisits', friends);
                         let colors, borders;
                         let viewBorders = localState.viewBorders ? true: false;
                         if (!localState.viewingFriend) {
@@ -62,8 +65,14 @@ export default class extends Component {
                           colors = oneFriend;
                           borders = visitsUser;
                         }
+                        if (loadinguser || loadingVisits || loadingFriendVisits) {
+                          return <div>loading</div>
+                        }
                         return (
+                          <div>
                           <DynamicMap colors={colors} borders={borders} viewBorders={viewBorders} />
+                          <Legend />
+                          </div>
                         )
                       }}
                     </Query>
