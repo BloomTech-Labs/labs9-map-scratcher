792519be902ae71be996f402d44db5d50dcbcc58
kyranmccann <kyran.mccann@gmail.com>
2019-01-23 00:25:14 -0800

    added comments

--- frontend/components/MapHeader/CountryDropdown.js
+++ frontend/components/MapHeader/CountryDropdown.js
@@ -6,15 +6,18 @@ import { QUERY_COUNTRIES_HEADER, MUTATION_OPENMODAL_TRAVELS } from '../../servic
 
 export default class CountriesDropdown extends Component {
   render() {
+    //query retrieves the list of all countries available to interact with. 
     return (
       <Query query={QUERY_COUNTRIES_HEADER}>
       {({ loading, data: { countries }}) => {
+        //retrieves the countries array from the query and maps through it to create the dropdown list data.
         const countriesList = countries.map(country => {
           return {
             text: country.name,
             value: country.id
           }
         })
+        //query returns a mutation that provides the onChange handler for dropdown selection: it writes the value of the selection to the apollo cache and also sets the modalOpen value to true to display the modal.
         return (
           <Mutation mutation={MUTATION_OPENMODAL_TRAVELS}>
             {(openModal, { data }) => (
