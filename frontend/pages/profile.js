bb5e8bc730d3b91d6c01491cc0d7f04de84dfafb
kyranmccann <kyran.mccann@gmail.com>
2019-01-22 16:47:26 -0800

    resolving conflicts

---
+++ frontend/pages/profile.js
@@ -0,0 +1,84 @@
+import Link from 'next/link'
+import Settings from '../components/Settings/settings.js'
+import { Query, Mutation } from 'react-apollo'
+import { Fragment } from 'react'
+import {
+  QUERY_CLIENT_PROFILE,
+  QUERY_USER_PROFILE,
+  QUERY_FRIENDS_PROFILE,
+  QUERY_USERS_PROFILE,
+  MUTATION_DELETEFRIEND_PROFILE
+} from '../services/requests'
+=======
+
+import ProfileCard from '../components/ProfileComponents/userProfile.js'
+
+
+export default () => (
+  <Fragment>
+    <p>
+      <Link href="/">
+        <a>Home </a>
+      </Link>
+      >
+      <Link href="/profile">
+        <a> Profile</a>
+      </Link>
+    </p>
+    <Query query={QUERY_CLIENT_PROFILE}>
+    {({ loading: loadingUserId, data: {userId} }) => {
+      return (
+        <Fragment>
+          <Query query={QUERY_USER_PROFILE} variables={{id: userId}}>
+          {({ loading: loadingUser, data: {user} }) => {
+            if (loadingUserId || loadingUser) return <div>Loading</div>
+            return (
+              <div>{user.name}</div>
+            )
+          }}
+          </Query>
+          <Query query={QUERY_FRIENDS_PROFILE} variables={{id: userId}}>
+          {({ loading: loadingFriends, data: {friends} }) => {
+            if (loadingUserId || loadingFriends) return <div>Loading</div>
+            return (
+              <div>
+                {
+                friends.map(friend => {
+                  return (
+                    <div>
+                      <span key={friend.id}>{friend.name}</span>
+                      <Mutation
+                        mutation={MUTATION_DELETEFRIEND_PROFILE}
+                        variables={{userId: userId, friendId: friend.id}}
+                      >
+                        {deleteFriend => (
+                            <button onClick={deleteFriend}>
+                                Click plz
+                            </button>
+                        )}
+                      </Mutation>
+                    </div>
+                  )
+                })
+                }
+              </div>
+            )
+          }}
+          </Query>
+        </Fragment>
+      )
+    }}
+    </Query>
+    <Query query={QUERY_USERS_PROFILE}>
+    {({ loading, data: {users} }) => {
+      if (loading) return <div>Loading</div>
+      return (
+        <div>{users.map(user => <div key={user.id}>{user.name}</div>)}</div>
+      )
+    }}
+    </Query>
+    <Settings />
+  </Fragment>
+    <ProfileCard />
+  </div>
+)
---
+++ frontend/services/requests.js
@@ -0,0 +1,289 @@
+import gql from 'graphql-tag'
+// naming scheme - query/mutation_target_page/component
+
+// >>>PROFILE PAGE
+// client requests
+export const QUERY_CLIENT_PROFILE = gql`
+  {
+    userId @client
+  }
+`
+// yoga requests
+export const QUERY_USER_PROFILE = gql`
+  query User($id: ID!) {
+    user(id: $id) {
+      name
+      nickname
+      email
+      scratchingAutomated
+      isPrivate
+    }
+  }
+`
+export const QUERY_USERS_PROFILE = gql`
+  {
+    users {
+      id
+      name
+    }
+  }
+`
+export const QUERY_FRIENDS_PROFILE = gql`
+  query User($id: ID!) {
+    friends(id: $id) {
+      id
+      name
+    }
+  }
+`
+export const MUTATION_UPDATEUSER_PROFILE = gql`
+  mutation UpdateUser(
+      $id: ID!
+      $name: String,
+      $nickname: String,
+      $email: String,
+      $scratchingAutomated: Boolean,
+      $isPrivate: Boolean)
+    {
+    updateUser(
+      name: $name,
+      nickname: $nickname,
+      email: $email,
+      scratchingAutomated: $scratchingAutomated,
+      isPrivate: $isPrivate)
+    {
+      name,
+      nickname,
+      email,
+      scratchingAutomated,
+      isPrivate
+    }
+  }
+`
+export const MUTATION_ADDFRIEND_PROFILE = gql`
+  mutation AddFriend(
+      $userId: ID!
+      $friendId: ID!)
+    {
+    addFriend(
+      userId: $userId,
+      friendId: $friendId,
+      )
+    {
+     userId,
+     friendId
+    }
+  }
+`
+export const MUTATION_DELETEFRIEND_PROFILE = gql`
+  mutation DeleteFriend(
+      $userId: ID!
+      $friendId: ID!)
+    {
+    deleteFriend(
+      userId: $userId,
+      friendId: $friendId,
+    )
+    {
+      id,
+    }
+  }
+`
+
+// >>>TRAVELS PAGE
+// client requests
+export const QUERY_CLIENT_TRAVELS = gql`
+  {
+    userId @client,
+    viewingFriend @client,
+    friendId @client
+  }
+`
+
+export const QUERY_CLIENT_VIEWFRIEND = gql`
+  {
+    viewingFriend @client,
+    friendId @client
+  }
+`
+
+// yoga requests
+export const QUERY_USERVISITS_TRAVELS = gql`
+    query User($id: ID!) {
+    user(id: $id) {
+      visits {
+        id
+        level
+        note
+        country {
+          id
+          name
+          code
+        }
+      }
+    }
+  }
+`
+export const QUERY_FRIENDSVISITS_TRAVELS = gql`
+query User($id: ID!) {
+  friends(id: $id) {
+    id
+    name
+    visits {
+      id
+      level
+      note
+      country {
+        id
+        name
+        code
+      }
+    }
+  }
+}
+`
+
+// >>>MAP HEADER (props: userId)
+// client requests
+// unfinished mutation to toggle viewingFriend
+// export const MUTATION_VIEWINGFRIEND_TRAVELS = gql`
+//   mutation ViewingFriend()
+//   {
+//     viewingFriend() @client
+//   }
+// `
+
+// need mutation to update friendId
+
+// yoga requests
+export const QUERY_COUNTRIES_HEADER = gql`
+  {
+    countries {
+      id
+      name
+      code
+    }
+  }
+`
+export const QUERY_FRIENDS_HEADER = gql`
+  query User($id: ID!) {
+    friends(id: $id) {
+      id
+      name
+    }
+  }
+`
+export const MUTATION_VIEWINGFRIEND_TRAVELS = gql`
+  mutation($id: ID!) {
+    viewFriend(id: $id) @client
+  }
+`
+
+// >>>MAP (props: userId, viewingFriend, friendId, visitsFriends, visitsUser)
+// no client requests
+// no yoga requests
+
+// >>>COUNTRY MODAL (props: userId, viewingFriend, friendId, countryName)
+// no client requests
+
+// yoga requests
+export const QUERY_COUNTRY_MODAL = gql`
+  query Country($name: String!) {
+    country(name: $name) {
+      id
+      name
+      code
+    }
+  }
+`
+
+export const MUTATION_CREATEVISIT_MODAL = gql`
+  mutation CreateVisit(
+      $userId: ID!
+      $countryId: ID!,
+      $level: Int,
+      $note: String)
+    {
+    createVisit(
+      userId: $userId,
+      countryId: $countryId,
+      level: $level,
+      note: $note)
+    {
+      level,
+      note
+    }
+  }
+`
+
+export const MUTATION_UPDATEVISIT_MODAL = gql`
+  mutation UpdateVisit(
+      $id: ID!
+      $level: Int,
+      $note: String)
+    {
+    updateVisit(
+      id: $id,
+      level: $level,
+      note: $note)
+    {
+      level,
+      note
+    }
+  }
+`
+export const MUTATION_DELETEVISIT_MODAL = gql`
+  mutation DeleteVisit(
+      $id: ID!)
+    {
+    deleteVisit(id: $id) {
+      id
+    }
+  }
+`
+
+// UNASSIGNED QUERIES & MUTATIONS - DO NOT DELETE
+
+
+// export const CREATEUSER_MUTATION = gql`
+//   mutation CreateUser(
+//       $twitterHandle: String,
+//       $name: String,
+//       $nickname: String,
+//       $email: String)
+//     {
+//     createUser(
+//       twitterHandle: $twitterHandle,
+//       name: $name,
+//       nickname: $nickname,
+//       email: $email)
+//     {
+//       name
+//     }
+//   }
+// `
+
+// export const DELETEUSER_MUTATION = gql`
+//   mutation DeleteUser(
+//       $id: ID!)
+//     {
+//     deleteUser(
+//       id: $id,
+//     )
+//     {
+//       id
+//     }
+//   }
+// `
+
+export const MUTATION_TOGGLE_SOMETHING = gql`
+  mutation toggleLoggedIn {
+    toggleLoggedIn @client
+  }
+`
+
+export const QUERY_CLIENT_LOGGED = gql`
+  query {
+     isLoggedIn @client
+   }
+`
