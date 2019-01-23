d288d89e8978b81b9bb6a67ee09b542c4f29e4bc
kyranmccann <kyran.mccann@gmail.com>
2019-01-23 00:54:59 -0800

    added viewBorders props logic

--- frontend/pages/travels.js
+++ frontend/pages/travels.js
@@ -36,7 +36,7 @@ export default class extends Component {
     render() {
         return this.makeQueriesAndRenderMap();
     }
-    displayMap(colors, borders) {
+    displayMap(colors, borders, viewBorders) {
         return (
             <React.Fragment>
                 <MapHeader />
@@ -44,6 +44,7 @@ export default class extends Component {
                     <DynamicMap
                         colors={colors}
                         borders={borders}
+                        viewBorders={viewBorders}
                     />
                     <Legend />
                 </div>
@@ -136,15 +137,19 @@ export default class extends Component {
         // Get data from response
         let error = response.error;
         let loading = response.loading;
-
+        //variables for the colors and borders and viewBorder boolean to be passed to the map
         let colors, borders;
 
+        let viewBorders = localState.viewBorders ? true : false;
+        console.log('in query', viewBorders)
+        //if the viewingFriend boolean on the apollo cache is false, sets the colors to the user's visits and the borders to the friends' visits
         if (!localState.viewingFriend) {
           colors = visitsUser;
           borders = fixData(response.data.friends);
         }
-
+        //if the viewingFriend boolean on the apollo cache is true, uses the friendId on the apollo cache to set the colors for the map to that single friend's visits and sets the borders to the user's visits.
         if (localState.viewingFriend && localState.friendId) {
+          //filters the friends response to return the single friend whose id matches the friend being viewed
           let oneFriend = response.data.friends.filter(friend => friend.id === localState.friendId)
           oneFriend = fixData(oneFriend);
           colors = oneFriend;
@@ -158,6 +163,6 @@ export default class extends Component {
             return this.displayError(error);
         }
         // Continue Rendering
-        return this.displayMap(colors, borders);
+        return this.displayMap(colors, borders, viewBorders);
     }
 }
