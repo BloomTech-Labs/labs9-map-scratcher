7e06a1cd77ec3bbda66ebd844ad90e22dc90f5c5
Lola <lolahef@gmail.com>
2019-01-24 16:34:33 -0500

    Added queries to scratcher

--- frontend/components/CountryViewModal/Scratcher.js
+++ frontend/components/CountryViewModal/Scratcher.js
@@ -1,24 +1,47 @@
 import React, { Component } from 'react'
 import Scatcher from '../Scratcher/index.js'
+import { Query } from 'react-apollo'
+import { QUERY_COUNTRYID_SCRATCHER, QUERY_USER_SCRATCHER } from '../../services/requests'
 
 export default class Scratcher extends Component {
+  
   render() {
+    const { countryId, displayId, disabled } = this.props
+    let scratchable = !disabled
     return (
       <div style={{height: '200px'}}>
-        {/* scratchable: nested ternary if user prop === self, then check automated settings */}
-        <Scatcher 
-          scratchable={this.props.disabled}
-          destination={`/static/country-alpha/usa.svg`} 
-          colorOutline={'cyan'} 
-          colorScratch={'silver'} 
-          handleScratchAll={() => console.log('working')} 
-          handleLoadingError={() => console.log('cannot load image')} 
-          style={{ height: '200px' }} />
+        <Query 
+          query={QUERY_COUNTRYID_SCRATCHER}
+          variables={{ id: countryId }}>
+          {({ loading: loadingCountryCode, data: {countryById} }) => {
+            return (
+              <Query 
+                query={QUERY_USER_SCRATCHER} 
+                variables={{ id: displayId }}>
+              {({ loading: loadingUserSetting, data: {user} }) => {
+                if (!disabled && !user.scratchingAutomated) scratchable = true
+                if (loadingCountryCode || loadingUserSetting) return <div>Loading</div>
+                return (
+                  <Scatcher 
+                  scratchable={scratchable}
+                  destination={countryById.code} 
+                  colorOutline={'cyan'} 
+                  colorScratch={'silver'} 
+                  handleScratchAll={() => console.log('working')} 
+                  handleLoadingError={() => console.log('cannot load image')} 
+                  style={{ height: '200px' }} />
+                )
+              }}
+              </Query>
+            )
+          }}
+        </Query>
       </div>
     )
   }
 }
 
+// Query country code from countryId props
 
 // props to receive:
 //   userType(string) - self or friend, determines if scratcher is scratchable and if visit level and note can be edited
