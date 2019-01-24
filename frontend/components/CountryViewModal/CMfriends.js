985fc9dfe4a974d293e14d747ab1a49686bacf7d
kyranmccann <kyran.mccann@gmail.com>
2019-01-24 04:54:15 -0800

    updated query logic

--- frontend/components/CountryViewModal/CMfriends.js
+++ frontend/components/CountryViewModal/CMfriends.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { List } from 'semantic-ui-react'
+import React from 'react';
+import { List } from 'semantic-ui-react';
 import { Query } from 'react-apollo';
 import { QUERY_CLIENT_PROFILE, QUERY_FRIENDSVISITS_TRAVELS } from '../../services/requests';
 
@@ -8,50 +8,50 @@ export default class CountryModalFriends extends React.Component {
     //map over the query, display friends and level of visit
     render() {
     return (
-        <Query query={QUERY_CLIENT_PROFILE}>
-        {({loading: loadingUser, data: { userId }}) => {
-          return (
-            <Query query={QUERY_FRIENDSVISITS_TRAVELS} variables={{ id: userId }}>
+            <Query query={QUERY_FRIENDSVISITS_TRAVELS} variables={{ id: this.props.displayId }}>
             {({ loading: loadingFriends , data: { friends }}) => {
-              let friendArray = [];
-              friends.map(friend => {
-                friend.visits.map(visit => {
-                  if (visit.country.id === "cjqy9e28y00i20840rwy5l1ti") {
-                    friendArray.push([friend.name, visit.level])
-                  }
+              let friendsList;
+              if (friends) {
+                let friendArray = [];
+                friends.map(friend => {
+                  friend.visits.map(visit => {
+                    if (visit.country.id === this.props.id) {
+                      friendArray.push([friend.name, visit.level])
+                    }
+                  })
                 })
-              })
-              friendArray = friendArray.sort((a,b) => {
-                return a[1] < b[1] ? 1 : -1
-              })
-              console.log(friendArray);
-              const icons = {
-                1: 'paper plane',
-                2: 'plane',
-                3: 'marker',
-                4: 'home'
+                friendArray = friendArray.sort((a,b) => {
+                  return a[1] < b[1] ? 1 : -1
+                })
+                const icons = {
+                  1: 'paper plane',
+                  2: 'plane',
+                  3: 'marker',
+                  4: 'home'
+                }
+                friendsList = friendArray.map(friend => {
+                    friend[1] = icons[friend[1]];
+                    return friend;
+                })
+                return friendsList;
               }
-              let friendsList = friendArray.map(friend => {
-                  friend[1] = icons[friend[1]];
-                  return friend;
-              })
+              console.log('list', friendsList);
               return (
-                <List >
+                <React.Fragment>
+                <List>
                   <List.Header>
                     Friends
                   </List.Header>
-                  {friendsList.map(person => {
+                  {friendsList && friendsList.map(person => {
+                    console.log('name', person[0])
                     return (
-                      <List.Item key={person[0]} icon={`${person[1]}`} content={`${person[0]}`}/>
+                      <List.Item key={person[0]} icon={person[1]} content={person[0]}/>
                     )
                   })}
                 </List>
+                </React.Fragment>
               )
             }}
             </Query>
-          )
-        }}
-        </Query>
-
     )}
 }
