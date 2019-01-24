a62da7ff8b2f1e93bdb6df9ccd9b04895e0bcd02
kyranmccann <kyran.mccann@gmail.com>
2019-01-23 16:47:09 -0800

    remove console.logs

--- frontend/pages/travels.js
+++ frontend/pages/travels.js
@@ -42,12 +42,9 @@ export default class extends Component {
             {({ loading: loadinguser, data }) => {
               const localState = data;
               const id = localState.userId;
-              console.log(id);
-              console.log({id});
               return (
                 <Query query={QUERY_USERVISITS_TRAVELS} variables={{id}}>
                 {({ loading: loadingVisits, data} ) => {
-                  console.log('uservisits', data)
                   let visitsUser = [];
                   visitsUser.push(data.user);
                   visitsUser = (fixData(visitsUser))
@@ -56,19 +53,13 @@ export default class extends Component {
                       {({ loading: loadingFriendVisits, data: { friends }}) => {
                         let colors, borders;
                         let viewBorders = localState.viewBorders ? true: false;
-                        console.log('is this asshole triggering', localState.friendID)
                         if (!localState.viewingFriend) {
-                          console.log('no friend')
                           colors = visitsUser;
                           borders = fixData(friends)
-                          console.log(borders)
                         }
                         if (localState.viewingFriend && localState.friendId) {
-                          console.log('view a friend')
                           let oneFriend = friends.filter(friend => friend.id === localState.friendId)
-                          console.log('in oneFriend', oneFriend);
                           oneFriend = fixData(oneFriend);
-                          console.log('singleFriend', oneFriend);
                           colors = oneFriend;
                           borders = visitsUser;
                         }
