729baf30d7b872c103d1a1b2d9a6279e5b828f88
kyranmccann <kyran.mccann@gmail.com>
2019-01-24 04:55:22 -0800

    updated render in hopes of easier less styling

--- frontend/components/CountryViewModal/CMheader.js
+++ frontend/components/CountryViewModal/CMheader.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Card, Button } from 'semantic-ui-react'
+import { Card, Button, Icon } from 'semantic-ui-react'
 import { Query, Mutation } from 'react-apollo'
 import { QUERY_COUNTRYID_MODAL, MUTATION_CLOSEMODAL_TRAVELS } from '../../services/requests'
 
@@ -13,16 +13,19 @@ export default class CMheader extends Component {
         {({ loading, data: {countryById} }) => {
           if (loading) return <div>Loading</div>
           return (
-            <Card.Header style={{paddingBottom: '5%'}}><i className='pe flag'/>
-            {countryById.name}
+            <Card.Header className='modal_header'>
+            <i className='pe flag'/>
+            <p>{countryById.name}</p>
               <Mutation mutation={MUTATION_CLOSEMODAL_TRAVELS}>
                 {closeModal => (
-                  <Button onClick={closeModal}>Return to Map</Button>
+                  <Button
+                  size='tiny' onClick={closeModal}>
+                  <Icon name="world"/>Return to Map</Button>
                 )}
               </Mutation>
             </Card.Header>
           )
-        }} 
+        }}
       </Query>
     )
   }
