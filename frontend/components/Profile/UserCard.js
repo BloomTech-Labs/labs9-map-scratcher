88283a1ca5b7976277de3589bb25086a7dc1c230
Lola <lolahef@gmail.com>
2019-01-28 11:01:28 -0500

    Fixed user card duplicate dependencies

--- frontend/components/Profile/UserCard.js
+++ frontend/components/Profile/UserCard.js
@@ -1,9 +1,3 @@
-import React, { Component } from 'react'
-import { Card, Image, Checkbox, Form, Input, Button } from 'semantic-ui-react'
-import { Mutation } from 'react-apollo'
-import { MUTATION_UPDATEUSER_PROFILE } from '../../services/requests/profile'
-import './profile.less'
-
 
 //== User Card =================================================================
 /*
@@ -14,7 +8,7 @@ import './profile.less'
 import React, { Component } from 'react';
 import { Card, Image, Checkbox, Form, Input, Button } from 'semantic-ui-react';
 import { Mutation } from 'react-apollo';
-import { MUTATION_UPDATEUSER_PROFILE } from '../../services/requests';
+import { MUTATION_UPDATEUSER_PROFILE } from '../../services/requests/profile';
 import './profile.less';
 
 
