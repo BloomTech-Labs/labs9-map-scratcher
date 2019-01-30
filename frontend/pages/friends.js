55d1acc7b7c93caa676ec13fe09ca4fb0f2136af
Lola <lolahef@gmail.com>
2019-01-29 19:11:23 -0500

    Fixed formatting

--- frontend/pages/friends.js
+++ frontend/pages/friends.js
@@ -1,30 +1,32 @@
 import FriendsCard from '../components/Friends/friendsCard.js'
-import { QUERY_CLIENT_PROFILE, QUERY_FRIENDS_PROFILE } from '../services/requests/profile.js'
+import { 
+  QUERY_CLIENT_PROFILE, 
+  QUERY_FRIENDS_PROFILE } from '../services/requests/profile'
 import { Query } from 'react-apollo';
 import { withRouter } from 'next/router'
 
 const Friends = withRouter((props) => (
-    <Query query={QUERY_CLIENT_PROFILE}>
-    {({ loading: loadingUserId, data: { userId } }) => {
-        if(loadingUserId) {
-            return <div>Loading...</div>
+  <Query query={QUERY_CLIENT_PROFILE}>
+  {({ loading: loadingUserId, data: { userId } }) => {
+    if(loadingUserId) {
+      return <div>Loading...</div>
+    }
+    return (
+      <Query query={QUERY_FRIENDS_PROFILE} variables={{id: userId}}> 
+      {({ loading, data: friends }) => {
+        if(loading) {
+          return <div>Loading...</div>
         }
         return (
-            <Query query={QUERY_FRIENDS_PROFILE} variables={{id: userId}}> 
-            {({ loading, data: friends }) => {
-                if(loading) {
-                    return <div>loading...</div>
-                }
-                return (
-                        <div>
-                            <FriendsCard id={props.router.query.id} currentUserId={userId} friendsData={friends.friends} />
-                        </div>
-                )
-            }}
-            </Query>
+          <div>
+            <FriendsCard currentUserId={userId} friendsData={friends.friends} />
+          </div>
         )
-    }}
-    </Query>
+      }}
+      </Query>
+    )
+  }}
+  </Query>
 ))
 
 export default Friends
