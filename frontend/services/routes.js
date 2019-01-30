6353ea0ef13318d0733a58cf9da8830ee2889613
Conner Hoessly <38872454+TRIF3X@users.noreply.github.com>
2019-01-30 13:51:29 -0600

    Update routes.js

--- frontend/services/routes.js
+++ frontend/services/routes.js
@@ -2,6 +2,6 @@ const routes = module.exports = require('next-routes')()
 
 
 routes
-// How to definte a route
+// How to define a route
 //.add('NAME', 'PATTERN = /name/:id', 'PAGE')
 .add('friends', '/friends/:id', 'friends')
