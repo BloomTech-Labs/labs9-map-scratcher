11be13045e677aed3e411abf68ea1ebc2d34df1e
TRIF3X <thewizardofcrypto@gmail.com>
2019-01-30 13:49:01 -0600

    document how to define a route

--- frontend/services/routes.js
+++ frontend/services/routes.js
@@ -1,4 +1,7 @@
 const routes = module.exports = require('next-routes')()
 
+
 routes
+// How to definte a route
+//.add('NAME', 'PATTERN = /name/:id', 'PAGE')
 .add('friends', '/friends/:id', 'friends')
