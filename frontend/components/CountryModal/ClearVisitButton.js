8d7cd2e1799d409fe4e7839fa4976f704fe86b6d
Lola <lolahef@gmail.com>
2019-02-01 15:49:47 -0500

    Added clear visit button

---
+++ frontend/components/CountryModal/ClearVisitButton.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+import { Query, Mutation } from 'react-apollo';
+import { 
+  MUTATION_DELETEVISIT_MODAL, 
+  QUERY_USERVISITS_MODAL } from '../../services/requests/modal';
+
+const ClearVisitButton = ({ countryId, userId, disabled }) => {
+  return (
+    disabled ? <Button>View Friend's Profile</Button> :
+    (
+      <Query query={QUERY_USERVISITS_MODAL} variables={{id: userId}}>
+        {({ loading, data: {user} }) => {
+          if (loading) return <div>Loading</div>
+          let visits = user.visits
+          let visit = visits.find(visit => visit.country.id === countryId)
+          let visitId = visit.id
+          return (
+            <Mutation 
+              mutation={MUTATION_DELETEVISIT_MODAL} 
+              variables={{ id: visitId }}
+              update={(cache, {data}) => {
+                const result = cache.readQuery({ query: QUERY_USERVISITS_MODAL, variables: {id: userId} });
+                const deletedVisit = data.deleteVisit
+                const visits = result.user.visits
+                cache.writeQuery({
+                  query: QUERY_USERVISITS_MODAL,
+                  variables: {id: userId},
+                  data: { visits: visits.filter(visit => visit.id !== deletedVisit.id) },
+                });
+              }}
+            >
+              {deleteVisit => (
+                <Button
+                  negative
+                  onClick={deleteVisit}
+                >Clear Visit</Button>
+              )}
+            </Mutation>
+          )
+        }}
+      </Query>
+    )
+  )
+}
+
+export default ClearVisitButton;
