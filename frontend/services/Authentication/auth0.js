b342bada838bf91be54f54a96e05c9bed9ac3b11
kyranmccann <kyran.mccann@gmail.com>
2019-02-04 13:28:35 -0800

    update files

--- frontend/services/Authentication/auth0.js
+++
@@ -1,6 +0,0 @@
-export const AUTH_CONFIG = {
-  api_audience: process.env.AUTH0_AUDIENCE,
-  domain: process.env.AUTH0_DOMAIN,
-  clientId: process.env.AUTH0_CLIENT_ID,
-  callbackUrl: 'http://localhost:1738/callback'
-}
