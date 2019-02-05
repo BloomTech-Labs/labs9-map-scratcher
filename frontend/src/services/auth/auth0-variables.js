6f493fcbc8eee69c1152e7c61a2a1e6dc6f1d10c
Lola <lolahef@gmail.com>
2019-02-05 17:25:32 -0500

    Sends issue

--- frontend/src/services/auth/auth0-variables.js
+++ frontend/src/services/auth/auth0-variables.js
@@ -3,4 +3,5 @@ export const AUTH_CONFIG = {
   domain: process.env.REACT_APP_AUTH0_DOMAIN,
   clientId: process.env.REACT_APP_AUTH0_CLIENT_ID,
   callbackUrl: process.env.NODE_ENV === 'development' ? 'http://localhost:3000/callback' : 'https://backpaca.surge.sh/callback',
+  issuer: process.env.REACT_APP_AUTH0_ISSUER
 }
