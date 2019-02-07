a554a4ed7a31cf6a68ed57908e9852fefb9f1fe7
Lola <lolahef@gmail.com>
2019-02-07 01:31:12 -0500

    Create visit distribution chart

---
+++ frontend/src/components/Profile/VisitsChart.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { PieChart, Pie, Cell, Label } from 'recharts';
+
+
+const VisitsChart = ({user}) => {
+  const distribution = {
+    wishlist: 0,
+    transited: 0,
+    visited: 0,
+    lived: 0
+  }
+  user.visits.map(visit => {
+    if (visit.level === 1) { distribution.wishlist++ }
+    if (visit.level === 2) { distribution.transited++ }
+    if (visit.level === 3) { distribution.visited++ }
+    if (visit.level === 4) { distribution.lived++ }
+  })
+  const data = [
+    {name: 'wishlist', value: distribution.wishlist}, 
+    {name: 'transited', value: distribution.transited},
+    {name: 'visited', value: distribution.visited}, 
+    {name: 'lived', value: distribution.lived}
+  ];
+  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+  
+  const RADIAN = Math.PI / 180;                    
+
+  const renderLabel = ({cx, cy, midAngle, innerRadius, outerRadius, percent, index}) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 1.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+  
+    const item = data[index];
+  
+    return (
+      <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central" key={`label-${item.value}`}>
+        {item.name}: {(percent*100).toFixed(0)}%
+      </text>
+    )
+  };
+  
+  return (
+    <PieChart width={800} height={400}>
+      <Pie
+        data={data} 
+        cx={300} 
+        cy={200} 
+        labelLine={true}
+        label={renderLabel}
+        outerRadius={80} 
+        fill="#8884d8"
+      >
+        {data.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]} key={index}/>)}
+      </Pie>
+    </PieChart>
+  )
+}
+
+export default VisitsChart
