1842aa2500d43dd847d7d46a8fb6c7d686003163
JacobABrennan <jacobabrennan@gmail.com>
2019-02-07 21:49:25 -0500

    Bug Fix: Scratch Handler allowed users to scratch current visit level

--- frontend/src/components/CountryModal/ScratchHandler.js
+++ frontend/src/components/CountryModal/ScratchHandler.js
@@ -135,14 +135,21 @@ export default class extends React.Component {
     // Put scratcher back in default state
     this.setState({itchy: false});
   }
-  handleChangeVisit = (newVisitLevel) => {
+  handleChangeVisit = (newVisitLevel, baseVisitLevel) => {
     /* handleChangeVisist is called whenever the user changes the visit level
     using the slider. It makes the map "itchy" but does not perform a mutation.
     */
-    this.setState({
-      itchy: true,
-      itchyLevel: newVisitLevel,
-    });
+    if(newVisitLevel === baseVisitLevel) {
+      this.setState({
+        itchy: false,
+        itchyLevel: undefined,
+      });
+    } else {
+      this.setState({
+        itchy: true,
+        itchyLevel: newVisitLevel,
+      });
+    }
   }
 
   //-- Rendering -----------------------------------
@@ -212,6 +219,7 @@ export default class extends React.Component {
                     )}</Mutation>
                   </div>
                   <VisitSlider
+                    baseVisitLevel={visitLevel}
                     visitLevel={displayLevel}
                     disabled={this.props.disabled}
                     onChange={this.handleChangeVisit}
@@ -232,6 +240,7 @@ export default class extends React.Component {
 
 /*-- Visit Slider ----------------------------------
   The slider expects the following props:
+    baseVisitLevel(number): The user's current visit level for this country
     onChange(function): a function to invoke when a new visit level is selected
     visitLevel(number): The current visit level selected, or from the database
     disabled(boolean): Whether or not to display the slider
@@ -257,8 +266,14 @@ function VisitSlider(props) {
         min={VISITLEVEL_NONE}
         max={VISITLEVEL_LIVED}
         onChange={(eventChange) => {
-          const newValue = Number(eventChange.currentTarget.value);
-          props.onChange(newValue);
+          let newValue = Number(eventChange.currentTarget.value);
+          if(
+            props.baseVisitLevel !== VISITLEVEL_NONE
+            && newValue === VISITLEVEL_NONE
+          ) {
+            newValue = 1;
+          }
+          props.onChange(newValue, props.baseVisitLevel);
         }}
       />
     </div>
